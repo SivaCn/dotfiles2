a1ae2506ddcee10805a1b093e78deea4eeec4848
Siva Cn <sivacn@biglittle.ai>
2024-09-02 23:40:13 +0530

    Adding machine setup script

---
+++ machine_setup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+apt -y install nala net-tools gedit nala git wget vim neovim htop i3 dmenu i3blocks rofi feh compton terminator \
+               pulseaudio alsa-tools materia-gtk-theme papirus-icon-theme lxappearance fonts-font-awesome polybar \
+               yad xdotool scrot openssh-server bashtop npm arandr espeak
+
+git clone --depth 1 https://github.com/wbthomason/packer.nvim ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+
+snap install slack dbeaver-ce postman
+
