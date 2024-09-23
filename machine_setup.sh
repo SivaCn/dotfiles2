8cd6a3637000b2cf7338592a1052c79effdd56f5
Siva Cn <sivacn@biglittle.ai>
2024-09-24 01:11:13 +0530

    Adding Screenshot tools

--- machine_setup.sh
+++ machine_setup.sh
@@ -2,11 +2,11 @@
 
 apt -y install nala net-tools gedit nala git wget vim neovim htop i3 dmenu i3blocks rofi feh compton terminator \
                pulseaudio alsa-tools materia-gtk-theme papirus-icon-theme lxappearance fonts-font-awesome polybar \
-               yad xdotool scrot openssh-server bashtop npm arandr espeak
+               yad xdotool scrot openssh-server bashtop npm arandr espeak imagemagick pdftk libreoffice \
+	       transmission-gtk vlc light gnome-screenshot
 
 git clone --depth 1 https://github.com/wbthomason/packer.nvim ~/.local/share/nvim/site/pack/packer/start/packer.nvim
 
 snap install slack dbeaver-ce postman
 
-apt install imagemagick pdftk
 
