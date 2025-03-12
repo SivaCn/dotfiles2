6582044882901ea238ec4d07e313e577e422f5d5
Cn Siva <cnsiva@protonmail.com>
2025-03-13 02:29:21 +0530

    updated battery monitor script

--- scripts/battery_monitor.sh
+++ scripts/battery_monitor.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 #
-# chmod +x scripts/battery_alert.sh
+# chmod +x scripts/battery_monitor.sh
 #
 # crontab -e
-# */5 * * * * ~/scripts/battery_alert.sh
+# */5 * * * * ~/scripts/battery_monitor.sh
 #
 
 # Function to check battery status and alert
@@ -17,13 +17,13 @@ check_battery() {
 
     # Alert for low battery (below 10%)
     if [ "$BATTERY_LEVEL" -lt 10 ] && [ "$BATTERY_STATE" == "discharging" ]; then
-        espeak "Battery is critically low" -s 150
+        espeak "Battery low" -s 150
         notify-send "Low Battery Warning" "Battery is at ${BATTERY_LEVEL}%. Plug in the charger!" -u critical
     fi
 
     # Alert for full charge (above 90% and charging)
     if [ "$BATTERY_LEVEL" -gt 95 ] && [ "$BATTERY_STATE" == "charging" ]; then
-        espeak "Battery is almost full" -s 150
+        espeak "Battery full" -s 150
         notify-send "Battery Full Alert" "Battery is at ${BATTERY_LEVEL}%. Unplug the charger!" -u normal
     fi
 }
